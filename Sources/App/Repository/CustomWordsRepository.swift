07cdbf5b2c9eb218876c81a0588b3b73869ec6b8
ldi_nsenba <n.senba@ldi.co.jp>
2025-06-05 11:31:05 +0900

    customWordsFile

---
+++ Sources/App/Repository/CustomWordsRepository.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  shirates-vision-server
+//
+//  Created by wave1008 on 2025/05/30.
+//
+
+import Foundation
+
+class CustomWordsRepository {
+    
+    var customWords: [String] = []
+    
+    
+}
